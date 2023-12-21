8b66f9655860b0391bc19e5539af3ac08a2849cc
ihtesham929 <ihtesham.nazir@codistan.org>
2023-12-21 18:07:08 +0500

    logo and name change

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,30 @@
 import './globals.css';
 export const metadata = {
-	title: 'Essolir',
-	description: 'Explore which lenes are right for you',
+  title: 'Belvin',
+  description: 'Explore which lenes are right for you',
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<head>
-				<link
-					href="https://fonts.googleapis.com/css?family=Noto+Sans"
-					rel="stylesheet"
-				/>
-				<link
-					rel="stylesheet"
-					type="text/css"
-					charset="UTF-8"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-				/>
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-				/>
-			</head>
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <head>
+        <link
+          href='https://fonts.googleapis.com/css?family=Noto+Sans'
+          rel='stylesheet'
+        />
+        <link
+          rel='stylesheet'
+          type='text/css'
+          charset='UTF-8'
+          href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+        />
+        <link
+          rel='stylesheet'
+          type='text/css'
+          href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
